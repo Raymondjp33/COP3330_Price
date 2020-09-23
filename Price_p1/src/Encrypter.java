d45d3ddec7ef28d953462474111edab24db5d5ed
Raymondjp33 <71288408+Raymondjp33@users.noreply.github.com>
2020-09-23 17:50:35 -0400

    Update Encrypter.java

--- Price_p1/src/Encrypter.java
+++ Price_p1/src/Encrypter.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Encrypter
@@ -10,20 +9,26 @@ public class Encrypter
 
         int num;
         String newCode;
-        int codeNew[] = new int[4];
+        int codeNew[];
 
         num = Integer.parseInt(code);
+        codeNew = number_scrambler(num);
+        newCode = Arrays.toString(codeNew);
+
+        return newCode;
+    }
+
+    public static int[] number_scrambler(int num)
+    {
+        int codeNew[] = new int[4];
 
         codeNew[0] = (((num / 10) % 10) + 7) % 10;
         codeNew[1] = ((num % 10) + 7) % 10;
         codeNew[2] = ((num / 1000) + 7) % 10;
         codeNew[3] = (((num / 100) % 10) + 7) % 10;
 
-
-        newCode = Arrays.toString(codeNew);
-
-
-        return newCode;
+        return codeNew;
     }
 
 }
+
